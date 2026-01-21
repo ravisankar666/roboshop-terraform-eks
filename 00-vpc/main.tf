77fd0f391339141147d6d45bf1725ac70bdb2eae
ravisankar666 <ravisankarp666@gmail.com>
2026-01-22 01:29:09 +0530

    add some change in repo

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/daws-86s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/ravisankar666/roboshop-terraform-eks.git"
     # VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
