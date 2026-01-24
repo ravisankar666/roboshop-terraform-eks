6db905f6744d0cc0d204d0945e44b3b9b3b1a979
ravisankar666 <ravisankarp666@gmail.com>
2026-01-25 00:27:04 +0530

    add some changes

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -12,7 +12,7 @@
 
 module "sg" {
   count = length(var.sg_names)
-  source = "git::https://github.com/ravisankar666/roboshop-terraform-eks.git"
+  source = "git::https://github.com/ravisankar666/terraform-aws-sg.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   sg_name = var.sg_names[count.index]
